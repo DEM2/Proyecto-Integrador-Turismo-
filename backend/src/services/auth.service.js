8f630327e358180c1ac4143c0cd70997bc8944a1
Luis Mejia <lmejiaidep@gmail.com>
2026-07-02 22:40:52 -0500

    conecté el front del register con el back y encripté la contraseña cuando la envio a la dB

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,12 +1,15 @@
 
 import pool from "../config/db.js";
+import bcrypt from "bcrypt";
 export async function register(userData) {
 
-     const result = await pool.query(
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const result = await pool.query(
         `INSERT INTO users (name, last_name, email, password, id_role, is_active)
          VALUES ($1, $2, $3, $4, $5, $6)
          RETURNING id, name, last_name, email, id_role, is_active, created_at`,
-        [userData.name, userData.last_name, userData.email, userData.password, 1, true]
+        [userData.name, userData.last_name, userData.email, hashedPassword, 1, true]
      );
 
 
