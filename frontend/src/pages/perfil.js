e18b9b40756f994176adcdfc37edeecd48734f00
luis <lmejiaidep@gmail.com>
2026-07-09 15:25:15 -0500

    seguimos con el diseño

--- frontend/src/pages/perfil.js
+++ frontend/src/pages/perfil.js
@@ -6,38 +6,42 @@ export function perfil() {
   return `
   ${NAV_BAR()}
     <main> 
-      <section class="h-48 grid lg:grid-cols-3 lg:grid-rows-1 grid-cols-1 grid-rows-2 ">
+      <section class="h-60 w-full relative ">
+        <img
+          class="w-full h-full object-cover"
+          src="/src/assets/img/hero3.png"
+          alt="Hero Image"
+        />
+      
+        <section class="absolute bottom-4 left-20 flex w-full ">
+          <img
+            class="size-50 rounded-full border-4 border-white object-cover z-10 "
+            src="/src/assets/img/familias.webp"
+            alt="Hero Image"
+          />
+          <div class="bg-black/40 text-white w-1/3 h-3/3.5 p-3 pl-22 rounded-3xl absolute top-5 left-30 z-0">
+          <div class="flex items-center self-center">
+            <h2 class="font-bold pr-3 text-2xl">
+            Mateo Mercado
+            
+          </h2>
+          <span class="bg-green-600 font-bold text-xs rounded-4xl p-1 pl-3 pr-3"> Explorador</span>
+          </div>
+          
+          <p class="text-xs"> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500sdssssssssssssssssssssssssssssssssssssssssssssssssss. </p>
+            <br>
+            <div> @mateomercado</div>
+          </div>
+          
+        </section>
+
+        <section class="bg-white absolute -bottom-10 rounded-2xl h-20 left-50 w-1/2 text-center border-2">
+      nsknsdf
+      </section>
+      </section>
+
 
       
-      <aside class="bg-amber-50 flex gap-1 pl-2  ">
-        <img
-            src="/src/assets/img/familias.webp"
-            class="size-48 rounded-full shrink-0 border-4 border-white  object-cover "
-            alt=""
-          />
-          <aside class="bg-amber-100 rounded-2xl p-2 cursor-pointer hover:bg-amber-200 text-center"> Instagram <br> Facebook <br> Tiktok</aside>
-          <article class="w-full flex flex-col items-center justify-center">
-          
-          <h2 class="text-lg font-medium ">
-            Mateo Mercado
-            <span class="text-xs block font-normal text-center">@Matooo_23432
-            
-            </span>
-          </h2>
-          <span class="text-green-600 font-medium ">Explorador 🌴</span>
-          </article>
-
-          <aside class="bg-amber-100 rounded-2xl p-2 cursor-pointer hover:bg-amber-200 text-center "> Seguir </aside>
-         
-      </aside>
-        
-
-        <img
-          src="/src/assets/img/hero3.png"
-          class="w-full h-full object-cover lg:col-span-2 "
-          alt=""
-        />
-      </section>
     
     </main>
   
@@ -46,8 +50,8 @@ export function perfil() {
 }
 
 export function perfilEvents() {
-  
-  
+
+
 
 
 }
