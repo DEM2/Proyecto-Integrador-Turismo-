17af6cbfc078db24ac0869419667c791fe3f793f
Daniel Mendoza <danielmendoza1515@gmail.com>
2026-07-11 23:09:23 -0500

    feat(destinations): add event category filters

--- frontend/src/components/filterCard.component.js
+++ frontend/src/components/filterCard.component.js
@@ -3,18 +3,18 @@ import { iconSVG } from "../utils/icons.js";
 export function filterCard(category) {
 
     return `
-    <button data-category="${category.name}" class="destination-filter flex min-w-56.25 items-center gap-3 rounded-2xl
+    <button data-category="${category.name}" class="destination-filter flex min-w-30 items-center gap-3 rounded-3xl
            border border-gray-200
-           bg-white px-4 py-3
+           bg-white px-3 py-2
            shadow-sm
            transition-all duration-300
            hover:border-blue-600 hover:shadow-md">
-    <span class="flex h-11 w-11 items-center justify-center rounded-full ${category.color} text-white">
+    <span class="flex h-8 w-8 items-center justify-center rounded-full ${category.color} text-white">
 
         ${iconSVG(category.icon,{
-        width:22,
-        height:22,
-        strokeWidth:2
+        width:18,
+        height:18,
+       
         })}
 
     </span>
