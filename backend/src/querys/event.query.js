6148ed27778752ff64c66e0efff1fbe7fa958f33
Luis Mejia <lmejiaidep@gmail.com>
2026-07-16 01:53:33 -0500

    se elimino location

--- backend/src/querys/event.query.js
+++ backend/src/querys/event.query.js
@@ -70,7 +70,7 @@ export async function createEventByUser(eventData) {
         id_user
     ) VALUES (
         $1, $2, $3, $4, $5, $6,
-        $7, $8, $9, $10, $11  
+        $7, $8, $9, $10 
     )
     RETURNING *
     `;
